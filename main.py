b435c17becd213216ef7de87d1ed349c717225b9
Prince Igweze <66817225+PrinceIgweze@users.noreply.github.com>
2024-07-28 14:13:06 -0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+import os, pyodbc, pandas as pd
+import cx_Oracle as cx
+
+conx = pyodbc.connect('Driver={SQL Server};Server=DESKTOP-022G187\SQLEXPRESS;Database=Retail;Trusted_Connection=yes;')
+#conx = cx.connect('username/password@IP:Port/DatabaseName')
+
+cursor = conx.cursor()
+query ="Select * from tblsales"
+df = pd.read_sql_query(query,conx)
+df.to_csv(os.path.join(os.getcwd(),"total_sales.csv"))
+
+
