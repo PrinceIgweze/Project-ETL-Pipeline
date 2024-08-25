be1d3da7a839d0e8a3107d7d67bda83fa7234d8e
Prince Igweze <66817225+PrinceIgweze@users.noreply.github.com>
2024-08-25 07:03:02 -0600

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import cx_Oracle as cx
 
 conx = pyodbc.connect('Driver={SQL Server};Server=DESKTOP-022G187\SQLEXPRESS;Database=Retail;Trusted_Connection=yes;')
 #conx = cx.connect('username/password@IP:Port/DatabaseName')
-
+ 
 cursor = conx.cursor()
 query ="Select * from tblsales"
 df = pd.read_sql_query(query,conx)
